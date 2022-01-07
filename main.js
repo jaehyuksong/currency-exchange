cf30681a315d339b26ce5afbc01d05043d9098a0
jaehyuksong <dan25874@gamil.com>
2022-01-07 11:29:47 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let currencyRatio= {
+    CAD:{
+        KRW:943.38,
+        CAD:1,
+        USD:0.78,
+        unit:"Dollar"
+    },
+    USD:{
+        KRW:1203.91,
+        CAD:1.28,
+        USD:1,
+        unit:"Dollar"
+    },
+    KRW:{
+        KRW:1,
+        CAD:0.0011,
+        USD:0.00083,
+        unit:"Won"
+    },
+}
