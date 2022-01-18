dfe97f35eb34005755f8eb87de7c2f2100d83760
jaehyuksong <dan25874@gmail.com>
2022-01-18 21:42:49 +0900

    third commit

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/*
+1. create 2 boxes
+2. create a drop-down list
+3. get exchange rate information
+4. enter the amount to be converted
+5. if you select an item from the drop-down list, it will be exchanged again according to the exchange rate
+*/
 let currencyRatio= {
     CAD:{
         KRW:943.38,
@@ -18,10 +25,28 @@ let currencyRatio= {
         unit:"Won"
     },
 }
-//console.log(currencyRatio.CAD.unit);
+let fromCurrency='CAD'
+let toCurrency='CAD'
 
-//console.log(currencyRatio["CAD"]["unit"])
-document.querySelectorAll("#from-currency-list a")
-.forEach((menu) => menu.addEventListener("click",function(){
-    document.getElementById("")
+document
+  .querySelectorAll("#from-currency-list a")
+  .forEach((menu) => menu.addEventListener("click",function(){
+      document.getElementById("from-button").textContent=this.textContent;
+      fromCurrency=this.textContent;
+      convert();
 }));
+
+document
+  .querySelectorAll("#to-currency-list a")
+  .forEach((menu) => menu.addEventListener("click",function(){
+      document.getElementById("to-button").textContent=this.textContent;
+      toCurrency=this.textContent;
+      convert();
+  }));
+
+  function convert() {
+    let amount=document.getElementById("from-input").value;
+    let convertedAmount = amount*currencyRatio[fromCurrency][toCurrency];
+    document.getElementById("to-input").value=convertedAmount;
+    };
+  
