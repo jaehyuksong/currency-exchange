e95263c39fbbdbeecc7345ab8a93273b7de3fda8
jaehyuksong <dan25874@gmail.com>
2022-01-19 19:11:33 +0900

    fourth commit

--- main.js
+++ main.js
@@ -5,48 +5,57 @@
 4. enter the amount to be converted
 5. if you select an item from the drop-down list, it will be exchanged again according to the exchange rate
 */
-let currencyRatio= {
-    CAD:{
-        KRW:943.38,
-        CAD:1,
-        USD:0.78,
-        unit:"Dollar"
+let currencyRatio = {
+    CAD: {
+        KRW: 943.38,
+        CAD: 1,
+        USD: 0.78,
+        unit: "Dollar",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/375px-Flag_of_Canada.svg.png"
     },
-    USD:{
-        KRW:1203.91,
-        CAD:1.28,
-        USD:1,
-        unit:"Dollar"
+    USD: {
+        KRW: 1203.91,
+        CAD: 1.28,
+        USD: 1,
+        unit: "Dollar",
+        img: "https://cdn-icons-png.flaticon.com/512/555/555526.png"
     },
-    KRW:{
-        KRW:1,
-        CAD:0.0011,
-        USD:0.00083,
-        unit:"Won"
+    KRW: {
+        KRW: 1,
+        CAD: 0.0011,
+        USD: 0.00083,
+        unit: "Won",
+        img: "https://cdn.countryflags.com/thumbs/south-korea/flag-400.png"
     },
 }
-let fromCurrency='CAD'
-let toCurrency='CAD'
+let fromCurrency = 'CAD'
+let toCurrency = 'CAD'
+let toButton = document.getElementById("to-button");
+let fromButton = document.getElementById("from-button");
+var unitWords = ["", "thousand", "million", "billion", "trillion"];
+var splitUnit = 1000;
 
 document
-  .querySelectorAll("#from-currency-list a")
-  .forEach((menu) => menu.addEventListener("click",function(){
-      document.getElementById("from-button").textContent=this.textContent;
-      fromCurrency=this.textContent;
-      convert();
-}));
+    .querySelectorAll("#from-currency-list a")
+    .forEach((menu) => menu.addEventListener("click", function () {
+        document.getElementById("from-button").textContent = this.textContent;
+        fromCurrency = this.textContent;
+        fromButton.innerHTML = `<img class="flag-img"src="${currencyRatio[fromCurrency].img}"/>${fromCurrency}`;
+        convert();
+    }));
 
 document
-  .querySelectorAll("#to-currency-list a")
-  .forEach((menu) => menu.addEventListener("click",function(){
-      document.getElementById("to-button").textContent=this.textContent;
-      toCurrency=this.textContent;
-      convert();
-  }));
+    .querySelectorAll("#to-currency-list a")
+    .forEach((menu) => menu.addEventListener("click", function () {
+        document.getElementById("to-button").textContent = this.textContent;
+        toCurrency = this.textContent;
+        toButton.innerHTML = `<img class="flag-img"src="${currencyRatio[toCurrency].img}"/>${toCurrency}`;
+        convert();
+    }));
 
-  function convert() {
-    let amount=document.getElementById("from-input").value;
-    let convertedAmount = amount*currencyRatio[fromCurrency][toCurrency];
-    document.getElementById("to-input").value=convertedAmount;
-    };
-  
+function convert() {
+    let amount = document.getElementById("from-input").value;
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency];
+    document.getElementById("to-input").value = convertedAmount;
+};
+
